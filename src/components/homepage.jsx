3151e8e25ddd52558ad8bfc8d1f4473a7188e623
beyarDev <beyarDev@gmail.com>
2022-07-26 08:53:23 +0100

    change the size of review img

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -18,17 +18,17 @@ export default function HomePage(){
              <section>
                 {reviewList.map(review =>{
                     return <div key={review.review_id} className='review-card'>
-                            <div className="flex-center review-img-owner-date-container">
-                            <img className="review-img" src={review.review_img_url} alt={review.title}></img>
+                            <div className="flex-center review-owner-date-container">
                             <span className="review-owner">{review.owner}</span>
                             <span className="review-date">{sliceDate(review.created_at)}</span>
                             </div>
                             <span className="review-title">{review.title}</span>
+                            <img className="review-img" src={review.review_img_url} alt={review.title}></img>
                             <span className="review-category"><span className="low-contrast">Category :</span> {review.category}</span>
                             <span className="review-designer"><span className="low-contrast">Designer :</span> {review.designer}</span>
                             <p className="review-body">{review.review_body}</p>
                             <div className="review-votes-container">
-                                <IncVotesBtn/>
+                                <IncVotesBtn reviewID={review.review_id}/>
                                 <span className="review-votes">{review.votes}</span>
                             </div>
                             <span className="review-comment-count">{review.comment_count} comments</span>
